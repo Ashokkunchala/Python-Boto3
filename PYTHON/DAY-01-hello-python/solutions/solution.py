3111e30de3b0e679da5d62b7f7141cc0e1eb902f
Kunchala Ashok <93498307+Ashokkunchala@users.noreply.github.com>
2026-09-20 14:49:57 +0530

    Add beginner lesson resources: PYTHON/DAY-01-hello-python/solutions/solution.py

---
+++ PYTHON/DAY-01-hello-python/solutions/solution.py
@@ -0,0 +1,9 @@
+name = "Ashok"
+role = "AWS DevOps Engineer"
+city = "Hyderabad"
+reason = "I want to automate AWS and DevOps tasks with Python."
+
+print("Name:", name)
+print("Role:", role)
+print("City:", city)
+print("Reason:", reason)
